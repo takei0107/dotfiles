68aba02db6268146bba539955a09ebb602046c39
takei0107 <takei0107.work@gmail.com>
2022-05-13 15:11:20 +0900

    update wezTerm.lua
    - font_size上げた
    - インデント調整

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,13 +3,10 @@ local wezterm = require('wezterm');
 return {
   default_cwd = wezterm.home_dir,
   font = wezterm.font(
-    "BitstreamVeraSansMono Nerd Font",
-    {
-      weight = "Bold",
-    }
+    "BitstreamVeraSansMono Nerd Font", { weight = "Regular", }
   ),
   use_ime = true,
-  font_size = 13.0,
+  font_size = 14.0,
   color_scheme = "Ayu Mirage",
   window_background_opacity = 0.8,
 }
