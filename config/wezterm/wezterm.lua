ac009a20dfde77cdda6b6ded7e9c706fef1748a1
takei0107 <takei0107.work@gmail.com>
2022-05-13 15:46:00 +0900

    update wezTerm.lua
    タブタイトルをプロセス名にした。

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,17 @@
 local wezterm = require('wezterm');
 
+local function basename(s)
+  return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local pane = tab.active_pane
+  local title = pane.pane_id .. ": " .. basename(pane.foreground_process_name)
+  return {
+    {Text=" " .. title .. " "},
+  }
+end)
+
 return {
   default_cwd = wezterm.home_dir,
   font = wezterm.font(
