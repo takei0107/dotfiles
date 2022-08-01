d726e036b8f0998ed9536276cfbe1007cc9ac078
takei0107 <takei0107.work@gmail.com>
2022-08-01 21:45:11 +0900

    lsp設定変更

--- config/nvim/lua/lsp/nvim-lsp.lua
+++ config/nvim/lua/lsp/nvim-lsp.lua
@@ -4,12 +4,24 @@ local mapping = require('lib.mapping')
 
 M.on_attach = function (client, bufnr)
 	local lsp_buf = vim.lsp.buf
-	mapping.noremap('n'):buffer(bufnr):silent():bind('gd', lsp_buf.definition)
-	mapping.noremap('n'):buffer(bufnr):silent():bind('gi', lsp_buf.implementation)
-	mapping.noremap('n'):buffer(bufnr):silent():bind('gr', lsp_buf.references)
+	--mapping.noremap('n'):buffer(bufnr):silent():bind('gd', lsp_buf.definition)
+	--mapping.noremap('n'):buffer(bufnr):silent():bind('gi', lsp_buf.implementation)
+	--mapping.noremap('n'):buffer(bufnr):silent():bind('gr', lsp_buf.references)
 	mapping.noremap('n'):buffer(bufnr):silent():bind('<leader>rn', lsp_buf.rename)
 	mapping.noremap('n'):buffer(bufnr):silent():bind('K', lsp_buf.hover)
 	mapping.noremap('n'):buffer(bufnr):silent():bind('<leader>ca', lsp_buf.code_action)
+
+	-- telescope builtin lsp functions
+	local telescope_builtin = require('telescope.builtin')
+	mapping.noremap('n'):buffer(bufnr):silent():bind('gd', telescope_builtin.lsp_definitions)
+	mapping.noremap('n'):buffer(bufnr):silent():bind('gi', telescope_builtin.lsp_implementations)
+	mapping.noremap('n'):buffer(bufnr):silent():bind('gr', telescope_builtin.lsp_references)
+	mapping.noremap('n'):buffer(bufnr):silent():bind('<leader>ds', telescope_builtin.diagnostics)
+
+	vim.cmd[[
+    autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
+    autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+	]]
 end
 
 -- よくわかっていない
