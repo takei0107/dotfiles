a2b66122b4ebcbfb397b0f7c638377b04910dacf
takei0107 <takei0107.work@gmail.com>
2022-08-15 14:12:21 +0900

    fix: nvim-lsp.lua
    - change lsp setting

--- config/nvim/lua/lsp/nvim-lsp.lua
+++ config/nvim/lua/lsp/nvim-lsp.lua
@@ -1,8 +1,6 @@
-local M = {}
-
 local mapping = require('lib.mapping')
 
-M.on_attach = function(client, bufnr)
+local on_attach = function(client, bufnr)
 	local lsp_buf = vim.lsp.buf
 	--mapping.noremap('n'):buffer(bufnr):silent():bind('gd', lsp_buf.definition)
 	--mapping.noremap('n'):buffer(bufnr):silent():bind('gi', lsp_buf.implementation)
@@ -29,15 +27,37 @@ end
 
 -- よくわかっていない
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local default_config = {
+	on_attach = on_attach,
+	capabilities = capabilities,
+}
+
+-- lsp setup
 require('mason').setup()
 require('mason-lspconfig').setup()
 require('mason-lspconfig').setup_handlers({
+
+	-- [[ default lsp config ]]
 	function(server_name)
 		require('lspconfig')[server_name].setup {
-			on_attach = M.on_attach,
-			capabilities = capabilities
+			on_attach = default_config.on_attach,
+			capabilities = default_config.capabilities,
+		}
+	end,
+
+	-- [[ lua lsp config ]]
+	["sumneko_lua"] = function()
+		require("lspconfig")["sumneko_lua"].setup {
+			on_attach = default_config.on_attach,
+			capabilities = default_config.capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { 'vim' }, -- vimのwarning抑止
+					},
+				}
+			},
 		}
 	end
 })
-
-return M
