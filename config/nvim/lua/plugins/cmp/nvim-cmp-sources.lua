b4a9df9ce563da4ea9f435f26e2f402d050cf281
takei0107 <takei0107.work@gmail.com>
2023-07-14 02:11:52 +0900

    fix: nvim-cmpのバッファ補完

--- config/nvim/lua/plugins/cmp/nvim-cmp-sources.lua
+++ config/nvim/lua/plugins/cmp/nvim-cmp-sources.lua
@@ -18,7 +18,16 @@ local sources = {
 	{
 		name = "hrsh7th/cmp-buffer",
 		sourceName = "buffer",
-		option = {},
+		option = {
+			-- ウィンドウ表示状態のバッファ
+			get_bufnrs = function()
+				local bufs = {}
+				for _, win in ipairs(vim.api.nvim_list_wins()) do
+					bufs[vim.api.nvim_win_get_buf(win)] = true
+				end
+				return vim.tbl_keys(bufs)
+			end,
+		},
 		format = function(vim_item)
 			vim_item.kind = "buffer"
 			return vim_item
