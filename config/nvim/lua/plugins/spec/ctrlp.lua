8f414b0702ec00282c584b938c73dd68f29acfe4
takei0107 <takei0107.work@gmail.com>
2023-07-14 01:53:35 +0900

    feat: ctrlpのキーマップ

--- config/nvim/lua/plugins/spec/ctrlp.lua
+++ config/nvim/lua/plugins/spec/ctrlp.lua
@@ -3,7 +3,10 @@ return {
 	init = function()
 		vim.g.ctrlp_map = "<leader>ff"
 		vim.g.ctrlp_working_path_mode = "w"
-		vim.g.ctrlp_cache_dir = vim.fn.stdpath('cache').."/ctrlp"
+		vim.g.ctrlp_cache_dir = vim.fn.stdpath("cache") .. "/ctrlp"
 		vim.g.ctrlp_show_hidden = 1
+
+		vim.keymap.set("n", "<leader>bb", "<cmd>CtrlPBuffer<cr>")
+		vim.keymap.set("n", "<leader>mm", "<cmd>CtrlPMRU<cr>")
 	end,
 }
