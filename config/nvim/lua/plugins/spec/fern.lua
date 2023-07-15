bedcc6560823730bbb5d215f4e4241d7c9f037b7
takei0107 <takei0107.work@gmail.com>
2023-07-16 03:06:24 +0900

    fix: fern settings

--- config/nvim/lua/plugins/spec/fern.lua
+++ config/nvim/lua/plugins/spec/fern.lua
@@ -2,8 +2,13 @@ return {
   "lambdalisue/fern.vim",
   lazy = true,
   init = function()
+    -- fern kemaps
     local fern_prefix = "<C-k>"
     vim.keymap.set("n", fern_prefix .. "f", "<Cmd>Fern . -reveal=% -opener=edit<CR>")
+
+    -- fern variablse
+    vim.g['fern#default_hidden'] = 1
+    vim.g['fern#default_exclude'] = ".git"
   end,
-  cmd = { "Fern", "FernReveal" },
+  cmd = { "Fern", "FernDo" },
 }
