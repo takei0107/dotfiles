cd497b3ab6b0dda991c8696fd1ac9a1d0cad3b20
takei0107 <takei0107.work@gmail.com>
2023-10-28 21:19:27 +0900

    fix: treesitter for c

--- config/nvim/lua/plugins/spec/nvim-treesitter.lua
+++ config/nvim/lua/plugins/spec/nvim-treesitter.lua
@@ -4,11 +4,11 @@ return {
   build = function()
     vim.cmd.TSUpdate()
   end,
-  ft = { "lua" },
+  ft = { "lua", "c" },
   ---@type fun(self:LazyPlugin, opts:table)|true
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua" },
+      ensure_installed = { "lua", "c" },
       sync_install = false,
       auto_install = false,
       highlight = {
